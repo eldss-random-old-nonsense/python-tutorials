2c3e7d92f80545078c731f6b341bed90a506e835
Evan <EvanLDouglass@gmail.com>
2020-03-22 16:19:23 -0700

    Add ocde to fetch data

--- sklearn/text_tutorial/sk_w_text.py
+++ sklearn/text_tutorial/sk_w_text.py
@@ -1,1 +1,24 @@
-# An example of working with text in sklearn
+'''An example of working with text in sklearn.'''
+
+from sklearn.datasets import fetch_20newsgroups
+
+# Limit categories for faster execution times
+categories = ['alt.atheism', 'soc.religion.christian',
+              'comp.graphics', 'sci.med']
+twenty_train = fetch_20newsgroups(subset='train',
+                                  categories=categories,
+                                  shuffle=True,
+                                  random_state=42)
+
+# Examples of accessing data from the object above
+print("Target categories:")
+print(twenty_train.target_names)
+print()
+print("Number of records by data in memory and by filenames:")
+print(len(twenty_train.data))
+print(len(twenty_train.filenames))
+print()
+print("First lines of the first file and its target category:")
+print("\n".join(twenty_train.data[0].split("\n")[:3]))
+print(twenty_train.target_names[twenty_train.target[0]])
+print()
